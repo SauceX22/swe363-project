39fddfa6dd2c3496206c1368d889b2a27ace3ca1
Mahmoud Alhussen <mahmoudzak4@gmail.com>
2024-11-03 22:23:35 +0300

    feat: market page (#1)
    * setup server stuff
    
    * Ahmed
    
    * fix: layout, added to route "/market"
    
    * chore: cleanup css
    
    * chore: quick layout changes

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,25 +1,24 @@
-import "../App.css";
-import Providers from "../components/config/providers";
-import "../index.css";
+import "@/App.css";
+import Providers from "@/components/config/providers";
 
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => {
     return (
-      <>
+      <body>
         <div className="flex gap-2 p-2">
           <Link to="/" className="[&.active]:text-orange-500">
             Home
           </Link>
         </div>
         <hr />
-        <Providers>
-          <Outlet />
-        </Providers>
-        <TanStackRouterDevtools />
-      </>
+        <main className="px-16 py-8">
+          <Providers>
+            <Outlet />
+          </Providers>
+        </main>
+      </body>
     );
   },
 });

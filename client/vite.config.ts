9cd0363e184cf11e41502a0f6f06e6636c13b6a6
SauceX <mahmoudzak4@gmail.com>
2024-12-09 16:45:57 +0300

    chore: vercel things

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [vercel(), react(), TanStackRouterVite()],
   envDir: path.resolve(__dirname, "../"),
   resolve: {
     alias: {
